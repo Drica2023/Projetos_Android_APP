d727719034dbc6d9d3de86743cabdbeac9ad9ad5
unknown <senai@NSN11922.edu119.local>
2023-12-07 16:54:16 -0300

    concluido a tela cadastro e criado tela recuperar senha

---
+++ OLXClone/app/src/main/java/senai/com/br/olxclone/autenticacao/RecuperarSenhaActivity.java
@@ -0,0 +1,16 @@
+package senai.com.br.olxclone.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import senai.com.br.olxclone.R;
+
+public class RecuperarSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_senha);
+    }
+}
